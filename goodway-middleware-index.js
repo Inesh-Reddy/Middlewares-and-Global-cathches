b41c21dfe106974b3cf4056f816d47d7ddfce693
Inesh-Reddy <ineshreddy007@gmail.com>
2024-06-14 13:04:13 +0530

    implementing the middleware

--- goodway-middleware-index.js
+++ goodway-middleware-index.js
@@ -2,15 +2,46 @@ const express = require('express');
 const app = express();
 const port  = 3000;
 
-app.get(('/health-checkup'), (req,res)=>{
+//middlewares
+const authMiddlewares = (req, res, next)=>{
+    const username = req.headers.username;
+    const password = req.headers.password;
+    if(username != 'inesh' && password != 'pass'){
+        res.status(403).json({
+            msg: "authentication failed"
+        })
+    }else{
+        next();
+    }
+}
+
+const inputvalMiddleware = (req, res, next) => {
+    const kidneyid = req.query.kidneyid;
+    if(kidneyid != 1 && kidneyid != 2){
+        res.status(403).json({
+            msg: "issue with inputs validation",
+        })
+    }else{
+        next();
+    }
+}
+
+
+app.get(('/health-checkup'),authMiddlewares, inputvalMiddleware, (req,res)=>{
     res.status(200).json({
         msg: "Your kidney is fine",
     })
 })
 
-app.post(('/kidney-replacement'), (req, res)=>{
+app.get(('/kidney-checkup'), authMiddlewares, inputvalMiddleware, (req, res)=>{
     res.status(200).json({
         msg: "Your kidney got replaced",
+    })
+})
+
+app.get(("/heart-checkup"), authMiddlewares, (req, res)=>{
+    res.status(200).json({
+        msg: "Your heart is fine",
     })
 })
 
