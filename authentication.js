d6511f751164fbbc146087feff313655ac3f8c8d
Inesh-Reddy <ineshreddy007@gmail.com>
2024-06-16 10:27:28 +0530

    authentication key points

---
+++ authentication.js
@@ -0,0 +1,6 @@
+/**
+ * Give the user back the token on signin/signup
+ * Ask the user to send back the requests
+ * when the user signout ask the user to forget the token {or revoke it from backend}
+ */
+
